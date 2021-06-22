3cb55f12597a5b7f933804757875e39cce971a18
livingstone-holidays <86318525+livingstone-holidays@users.noreply.github.com>
2021-06-22 23:46:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+const config = {
+  style: "mapbox://styles/mapbox/streets-v11",
+  accessToken:
+    "pk.eyJ1IjoibGl2aW5nc3RvbmVzdGF5cyIsImEiOiJja3EyOGZoNTEwbTJ0MndwODVvdmlsMzNpIn0.1RIytc5O3-uTZHCjysTAuw",
+  csv: "./data.csv",
+  center: [30.462,80.115],
+  zoom: 6.5,
+  title: "LivingStone Stays",
+  description:
+    "Please find a perfect LivingStone stay for your next holiday.You can use filters to narrow down the properties based on your preferences.",
+  sideBarInfo: ["Property Name", "Region", "Activities"],
+  popupInfo: ["Property Name"],
+  filters: [
+    {
+      type: "dropdown",
+      title: "Region ",
+      columnHeader: "Region",
+      listItems: [
+        "Tirthan",
+        "Alibaug",
+        "Bhimtal",
+        "Chail",
+        "Tirthan",
+        "Shimla",
+        "Chail",
+        "Bir",
+      ],
+    },
+    {
+      type: "dropdown",
+      title: "Activities: ",
+      columnHeader: "Activities",
+      listItems: [
+        "Bonfire",
+        "Trek",
+        "Pets",
+      ],
+    },
+  ],
+};
